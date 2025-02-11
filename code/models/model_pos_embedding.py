0ba1f213a44ca88e49fb3d96ad30a975872b1e68
larry-adesola <adesola.emmanuel@yahoo.com>
2025-02-11 17:00:09 +0000

    project repo restructure

--- code/models/model_pos_embedding.py
+++ code/models/model_pos_embedding.py
@@ -3,27 +3,7 @@ import torch.nn as nn
 from transformers import RobertaModel, BertPreTrainedModel, RobertaConfig
 from copy import deepcopy
 #from transformers.models.roberta.modeling_roberta import RobertaClassificationHead
-
-class RobertaClassificationHead(nn.Module):
-  """Head for sentence-level classification tasks."""
-
-  def __init__(self, config):
-    super().__init__()
-    self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-    classifier_dropout = (
-        config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-    )
-    self.dropout = nn.Dropout(classifier_dropout)
-    self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
-
-  def forward(self, features, **kwargs):
-    x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
-    x = self.dropout(x)
-    x = self.dense(x)
-    x = torch.tanh(x)
-    x = self.dropout(x)
-    x = self.out_proj(x)
-    return x
+from models.classify_head import RobertaClassificationHead
 
 
 
